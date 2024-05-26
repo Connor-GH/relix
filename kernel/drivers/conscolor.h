e5e5004333b352556e3cb7f9b8ed3c2e0c875d49
Connor <connor-gh@outlook.com>
2024-05-25 22:05:37 -0500

    gitignore oops

---
+++ kernel/drivers/conscolor.h
@@ -0,0 +1,20 @@
+#pragma once
+
+enum {
+  BLACK,
+  BLUE,
+  GREEN,
+  CYAN,
+  RED,
+  MAGENTA,
+  BROWN,
+  GREY,
+  DARK_GREY,
+  BRIGHT_BLUE,
+  BRIGHT_GREEN,
+  BRIGHT_CYAN,
+  BRIGHT_RED,
+  BRIGHT_MAGENTA,
+  YELLOW, /* sometimes brown */
+  WHITE,
+};

16b8c385ba93d32a58c005110bba4f968e97db73
Connor-GH <connor-gh@outlook.com>
2024-09-03 14:39:31 -0500

    Update autogenerated headers

--- kernel/include/compiler_information.h
+++
@@ -1,5 +0,0 @@
-#pragma once
- #define XV6_COMPILER "gcc"
- #define XV6_COMPILER "gcc (Gentoo 13.3.1_p20240614 p17) 13.3.1 20240614"
- #define XV6_LINKER "ld"
- #define XV6_LINKER "GNU ld (Gentoo 2.42 p3) 2.42.0"

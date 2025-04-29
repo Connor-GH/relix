Connor-GH/relix

commit bec35cb5e809794d57b7e706dd29b7093ccb429d

Replace int $x40 syscalls with the x86 syscall/sysret pair
preliminary benchmarks show a performance improvement of 30%. Right now, release builds are broken due to a page fault in init, but it will be fixed next commit.

Makefile
@@ -38,9 +38,12 @@ ifneq ($(LLVM),)
 	LINKER_FLAGS = -fuse-ld=lld
 	WNOGCC =
 ifneq ($(ANALYZER),)
-	ANALYZER = --analyze
+	FUZZFLAGS = --analyze
 endif
 else
+ifneq ($(ANALYZER),)
+	FUZZFLAGS = -fanalyzer -Wno-analyzer-va-arg-type-mismatch
+endif
 	LLVM = 0
 	WNOGCC = -Wno-clobbered
 endif
@@ -52,7 +55,7 @@ WNOFLAGS = -Wno-unused-parameter -Wno-infinite-recursion -Wno-pointer-arith -Wno
 
 CFLAGS = -std=gnu11 -pipe -fno-pic -static -fno-builtin -ffreestanding \
 				 -fno-strict-aliasing -nostdlib -Og -ggdb -fno-omit-frame-pointer \
-				 -nostdinc $(ARCHNOFLAGS) $(WFLAGS) $(WNOFLAGS) $(WNOGCC) $(ANALYZER)
+				 -nostdinc $(ARCHNOFLAGS) $(WFLAGS) $(WNOFLAGS) $(WNOGCC) $(FUZZFLAGS)
 CFLAGS += $(shell $(CC) -fno-stack-protector -E -x c /dev/null >/dev/null 2>&1 && echo -fno-stack-protector)
 RUSTFLAGS = -Ctarget-feature=-avx,-avx2,-sse,-sse2,-sse3,-ssse3,-sse4.1,-sse4.2 -Crelocation-model=static -Cpanic=abort -Copt-level=0 -Ccode-model=kernel -Cno-redzone=true -Cincremental=true -Cembed-bitcode=n -Cforce-unwind-tables=n -Ccodegen-units=1 -Csymbol-mangling-version=v0 -Zfunction-sections=n
 CARGO_FLAGS = -Zunstable-options --target x86_64-unknown-none
@@ -68,8 +71,8 @@ else
 endif
 ASFLAGS = -gdwarf-2 -Wa,-divide --mx86-used-note=no
 
-CFLAGS += -m64 -march=x86-64 -mcmodel=kernel -mtls-direct-seg-refs -DX86_64=1
-ASFLAGS += -m64 -march=x86-64 -mcmodel=kernel -mtls-direct-seg-refs -DX86_64=1
+CFLAGS += -m64 -march=x86-64 -mtls-direct-seg-refs -DX86_64=1
+ASFLAGS += -m64 -march=x86-64 -mtls-direct-seg-refs -DX86_64=1
 
 # FreeBSD ld wants ``elf_x86_64_fbsd''
 ifeq ($(HOST_OS),FreeBSD)

kernel/Makefile
@@ -20,7 +20,9 @@ KERNEL_INC = $(IVARS) \
 						 -I$(KERNELDIR)/include \
 						 -I$(KERNELDIR)/drivers/include \
 						 -I$(KERNELDIR)/drivers \
-						 $(KERNEL_EXTRAS)
+							$(KERNEL_EXTRAS)
+
+KCFLAGS += -mcmodel=kernel
 
 
 include $(KERNELDIR)/boot/Makefile

commit 1db0136d246530bd99d21ddbea9ddfa43f470ca2

Fix syscall regression from commit bec35cb

Makefile
@@ -71,7 +71,7 @@ else
 endif
 ASFLAGS = -gdwarf-2 -Wa,-divide --mx86-used-note=no
 
-CFLAGS += -m64 -march=x86-64 -mtls-direct-seg-refs -DX86_64=1
+CFLAGS += -m64 -march=x86-64 -mtls-direct-seg-refs -DX86_64=1 -D__relix__=1
 ASFLAGS += -m64 -march=x86-64 -mtls-direct-seg-refs -DX86_64=1
 
 # FreeBSD ld wants ``elf_x86_64_fbsd''

include/errno.h
@@ -45,7 +45,7 @@ extern int errno;
 #define ENOTEMPTY 39 /* Directory not empty */
 #define ELOOP 40 /* Too many symbolic links */
 #define MAX_ERRNO 40
-#if !defined(__KERNEL__) && !defined(__ASSEMBLER__)
+#if !defined(__ASSEMBLER__)
 static const char *const errno_codes[] = {
 	[0] = "Success",
 	[EPERM] = "Operation not permitted",

kernel/Makefile
@@ -11,7 +11,7 @@ KERNEL_DRIVERS = $(KERNELDIR)/drivers/kbd.c \
 								 $(KERNELDIR)/drivers/ps2mouse.c
 KERNEL_OBJS = $(KERNEL_SRC:$(KERNELDIR)/%.c=$(BIN)/%.o) $(KERNEL_ASM:$(KERNELDIR)/%.S=$(BIN)/%.o) \
 	$(KERNEL_DRIVERS:$(KERNELDIR)/drivers/%.c=$(BIN)/%.o)
-KERNEL_EXTRAS = -D__KERNEL__=1 -D__relix__=1
+KERNEL_EXTRAS = -D__KERNEL__=1
 
 ifneq ($(KUBSAN),)
 	KUBSAN_FLAGS = -fsanitize=undefined

kernel/include/trap.h
@@ -1,10 +1,13 @@
 #pragma once
 #if __KERNEL__
 #include "time.h"
+#include "proc.h"
 void
 idtinit(void);
 extern time_t ticks;
 void
 tvinit(void);
 extern struct spinlock tickslock;
+void
+regdump(struct trapframe *tf);
 #endif

kernel/syscall.c
@@ -385,8 +385,8 @@ syscall_do(void)
 		"popq %%rbp\n"
 		"popq %%rsi\n"
 		"popq %%rdi\n"
-		"popq %%r9\n"
 		"popq %%r8\n"
+		"popq %%r9\n"
 		"popq %%r10\n"
 		"popq %%r11\n"
 		"popq %%r12\n"
@@ -438,8 +438,12 @@ syscall(void)
 			yticks = ticks;
 			release(&tickslock);
 
-			cprintf("pid %d: syscall %s => %ld (%lu ticks)\n", curproc->pid,
+			cprintf("pid %d: syscall %s => %ld (%lu ticks)", curproc->pid,
 							syscall_names[num], curproc->tf->rax, yticks - xticks);
+			if ((signed)curproc->tf->rax < 0) {
+				cprintf(" \"%s\"", errno_codes[-(signed)curproc->tf->rax]);
+			}
+			cprintf("\n");
 		} else {
 			curproc->tf->rax = syscalls[num]();
 		}

kernel/sysfile.c
@@ -87,8 +87,7 @@ sys_dup(void)
 	struct file *f;
 	int fd;
 
-	if (argfd(0, NULL, &f) < 0)
-		return -1;
+	PROPOGATE_ERR(argfd(0, NULL, &f));
 	if ((fd = fdalloc(f)) < 0)
 		return -EBADF;
 	filedup(f);

kernel/trap.c
@@ -60,6 +60,22 @@ decipher_page_fault_error_code(uint64_t error_code)
 		uart_printf("Caused by an SGX violation.\n");
 	}
 }
+
+void
+regdump(struct trapframe *tf)
+{
+	uart_printf("Register dump:\n");
+	uart_printf(
+		"rax = %#018lx rbx = %#018lx rcx = %#018lx rdx = %#018lx rsi = %#018lx\n"
+				 "rdi = %#018lx rsp = %#018lx rbp = %#018lx r8  = %#018lx r9  = %#018lx\n"
+				 "r10 = %#018lx r11 = %#018lx r12 = %#018lx r13 = %#018lx r14 = %#018lx\n"
+				 "r15 = %#018lx rip = %#018lx cs  = %#018lx ds  = %#018lx rfl = %#018lx\n"
+				 "err = %#018lx tno = %#018lx\n",
+
+							tf->rax, tf->rbx, tf->rcx, tf->rdx, tf->rsi, tf->rdi, tf->rsp,
+							tf->rbp, tf->r8, tf->r9, tf->r10, tf->r11, tf->r12, tf->r13,
+							tf->r14, tf->r15, tf->rip, tf->cs, tf->ds, tf->rflags, tf->err, tf->trapno);
+}
 static void
 decipher_error_code_nonpagefault(uint64_t error_code)
 {
@@ -88,8 +104,6 @@ decipher_error_code_nonpagefault(uint64_t error_code)
 	}
 	uart_printf("In the index: %lu\n", (error_code & 0x0000FFFF) >> 3);
 }
-
-
 
 void
 trap(struct trapframe *tf)
@@ -139,15 +153,22 @@ trap(struct trapframe *tf)
 	case T_IRQ0 + 7:
 	case T_IRQ0 + IRQ_SPURIOUS:
 		uart_printf("cpu%d: spurious interrupt at %#lx:%#lx\n", my_cpu_id(), tf->cs,
-						tf->rip);
+								tf->rip);
 		lapiceoi();
+		break;
+	case T_BRKPT:
+	case T_DEBUG:
+		uart_printf("cpu%d: breakpoint/trap at %#lx:%#lx\n", my_cpu_id(), tf->cs,
+								tf->rip);
+		regdump(tf);
+		kill(myproc()->pid, SIGTRAP);
 		break;
 	case T_ILLOP:
 		uart_printf("Illegal instruction\n");
-		uart_printf("from cpu %d rip %lx (cr2=%#lx)\n",
-			my_cpu_id(), tf->rip, rcr2());
+		uart_printf("from cpu %d rip %lx (cr2=%#lx)\n", my_cpu_id(), tf->rip,
+								rcr2());
 		if ((tf->cs & 3) == DPL_USER) {
-			kill(myproc()->killed, SIGILL);
+			kill(myproc()->pid, SIGILL);
 		} else {
 			panic("Illegal instruction in the kernel!");
 		}
@@ -155,14 +176,15 @@ trap(struct trapframe *tf)
 	case T_GPFLT:
 		uart_printf("General protection fault\n");
 		if ((tf->cs & 3) == DPL_USER) {
-			kill(myproc()->killed, SIGSEGV);
-			uart_printf("Process %s killed with SIGSEGV: sp=%#lx\n", myproc()->name, tf->rsp);
-			uart_printf("from cpu %d rip %lx (cr2=%#lx)\n",
-							my_cpu_id(), tf->rip, rcr2());
+			kill(myproc()->pid, SIGSEGV);
+			uart_printf("Process %s killed with SIGSEGV: sp=%#lx\n", myproc()->name,
+									tf->rsp);
+			uart_printf("from cpu %d rip %lx (cr2=%#lx)\n", my_cpu_id(), tf->rip,
+									rcr2());
 		} else {
 			uart_printf("BUG: General protection fault in the kernel!\n");
-			uart_printf("from cpu %d rip %lx (cr2=%#lx)\n",
-							my_cpu_id(), tf->rip, rcr2());
+			uart_printf("from cpu %d rip %lx (cr2=%#lx)\n", my_cpu_id(), tf->rip,
+									rcr2());
 			panic("kernel general protection fault");
 		}
 		break;
@@ -178,36 +200,39 @@ trap(struct trapframe *tf)
 		if ((tf->cs & DPL_USER) == 0) {
 			panic("trap");
 		} else {
-			uart_printf("Process %s killed with SIGSEGV: sp=%#lx\n", myproc()->name, tf->rsp);
+			uart_printf("Process %s killed with SIGSEGV\n", myproc()->name);
+			regdump(tf);
 			uintptr_t pcs[10];
 			getcallerpcs_with_bp(pcs, &tf->rbp, 10);
 			for (int i = 0; i < 10; i++)
-					uart_printf("%#lx ", pcs[i]);
-				uart_printf("\n");
+				uart_printf("%#lx ", pcs[i]);
+			uart_printf("\n");
 			kill(myproc()->pid, SIGSEGV);
 		}
 		break;
 	case T_DIVIDE:
-		uart_printf("%s[%d]: trap divide by zero error: %#lx\n", myproc()->name, myproc()->pid, tf->rip);
+		uart_printf("%s[%d]: trap divide by zero error: %#lx\n", myproc()->name,
+								myproc()->pid, tf->rip);
 		kill(myproc()->pid, SIGFPE);
 		break;
 	case T_SIMDERR:
 	case T_FPERR:
-		uart_printf("%s[%d]: floating point error: %#lx\n", myproc()->name, myproc()->pid, tf->rip);
+		uart_printf("%s[%d]: floating point error: %#lx\n", myproc()->name,
+								myproc()->pid, tf->rip);
 		kill(myproc()->pid, SIGFPE);
 		break;
 	default:
 		if (myproc() == NULL || (tf->cs & 3) == 0) {
 			// In kernel, it must be our mistake.
 			uart_printf("unexpected trap %ld from cpu %d rip %lx (cr2=%#lx)\n",
-							tf->trapno, my_cpu_id(), tf->rip, rcr2());
+									tf->trapno, my_cpu_id(), tf->rip, rcr2());
 			panic("trap");
 		}
 		// In user space, assume process misbehaved.
 		uart_printf("pid %d %s: trap %ld err %ld on cpu %d "
-						"rip %#lx addr %#lx--kill proc\n",
-						myproc()->pid, myproc()->name, tf->trapno, tf->err, my_cpu_id(),
-						tf->rip, rcr2());
+								"rip %#lx addr %#lx--kill proc\n",
+								myproc()->pid, myproc()->name, tf->trapno, tf->err, my_cpu_id(),
+								tf->rip, rcr2());
 		myproc()->killed = 1;
 	}
 

userspace/fcntl.c
@@ -17,7 +17,7 @@ open(const char *pathname, int flags, ...)
 	} else {
 		mode = 0;
 	}
-	return __syscall3(SYS_open, (long)pathname, flags, mode);
+	return __syscall_ret(__syscall3(SYS_open, (long)pathname, flags, mode));
 }
 
 int
@@ -45,5 +45,5 @@ fcntl(int fd, int cmd, ...)
 		arg = NULL;
 		break;
 	}
-	return __syscall3(SYS_fcntl, fd, cmd, (long)arg);
+	return __syscall_ret(__syscall3(SYS_fcntl, fd, cmd, (long)arg));
 }

userspace/include/libc_syscalls.h
@@ -1,121 +1,96 @@
 #pragma once
 #include <sys/syscall.h>
+#include "lib/compiler_attributes.h"
 
 extern int errno;
 
-#define __syscall0(num)                             \
-	({                                                \
-		long long __ret;                                \
-		__asm__ __volatile__("syscall"                  \
-												 : "=a"(__ret)              \
-												 : "a"(num)                 \
-												 : "rcx", "r11", "memory"); \
-		if (__ret < 0) {                                \
-			errno = -__ret;                               \
-			__ret = -1;                                   \
-		}                                               \
-		__ret;                                          \
-	})
+static __always_inline long
+__syscall_ret(unsigned long r)
+{
+	if (r > -4096UL) {
+		errno = -r;
+		return -1;
+	}
+	return r;
+}
 
-#define __syscall1(num, a1)                            \
-	({                                                   \
-		long long __ret;                                   \
-		register __typeof__(a1) __rdi __asm__("rdi") = a1; \
-		__asm__ __volatile__("syscall"                     \
-												 : "=a"(__ret)                 \
-												 : "a"(num), "r"(__rdi)        \
-												 : "rcx", "r11", "memory");    \
-		if (__ret < 0) {                                \
-			errno = -__ret;                               \
-			__ret = -1;                                   \
-		}                                               \
-		__ret;                                             \
-	})
-#define __syscall2(num, a1, a2)                        \
-	({                                                   \
-		long long __ret;                                   \
-		register __typeof__(a1) __rdi __asm__("rdi") = a1; \
-		register __typeof__(a2) __rsi __asm__("rsi") = a2; \
-		__asm__ __volatile__("syscall"                     \
-												 : "=a"(__ret)                 \
-												 : "a"(num), "r"(__rdi)        \
-												 : "rcx", "r11", "memory");    \
-		if (__ret < 0) {                                \
-			errno = -__ret;                               \
-			__ret = -1;                                   \
-		}                                               \
-		__ret;                                             \
-	})
-#define __syscall3(num, a1, a2, a3)                                     \
-	({                                                                    \
-		long long __ret;                                                    \
-		register __typeof__(a1) __rdi __asm__("rdi") = a1;                  \
-		register __typeof__(a2) __rsi __asm__("rsi") = a2;                  \
-		register __typeof__(a3) __rdx __asm__("rdx") = a3;                  \
-		__asm__ __volatile__("syscall"                                      \
-												 : "=a"(__ret)                                  \
-												 : "a"(num), "r"(__rdi), "r"(__rsi), "r"(__rdx) \
-												 : "rcx", "r11", "memory");                     \
-		if (__ret < 0) {                                \
-			errno = -__ret;                               \
-			__ret = -1;                                   \
-		}                                               \
-		__ret;                                                              \
-	})
-#define __syscall4(num, a1, a2, a3, a4)                                  \
-	({                                                                     \
-		long long __ret;                                                     \
-		register __typeof__(a1) __rdi __asm__("rdi") = a1;                   \
-		register __typeof__(a2) __rsi __asm__("rsi") = a2;                   \
-		register __typeof__(a3) __rdx __asm__("rdx") = a3;                   \
-		register __typeof__(a4) __r10 __asm__("r10") = a4;                   \
-		__asm__ __volatile__("syscall"                                       \
-												 : "=a"(__ret)                                   \
-												 : "a"(num), "r"(__rdi), "r"(__rsi), "r"(__rdx), \
-													 "r"(__r10),                                   \
-												 : "rcx", "r11", "memory");                      \
-		if (__ret < 0) {                                \
-			errno = -__ret;                               \
-			__ret = -1;                                   \
-		}                                               \
-		__ret;                                                               \
-	})
-#define __syscall5(num, a1, a2, a3, a4, a5)                              \
-	({                                                                     \
-		long long __ret;                                                     \
-		register __typeof__(a1) __rdi __asm__("rdi") = a1;                   \
-		register __typeof__(a2) __rsi __asm__("rsi") = a2;                   \
-		register __typeof__(a3) __rdx __asm__("rdx") = a3;                   \
-		register __typeof__(a4) __r10 __asm__("r10") = a4;                   \
-		register __typeof__(a5) __r8 __asm__("r8") = a5;                     \
-		__asm__ __volatile__("syscall"                                       \
-												 : "=a"(__ret)                                   \
-												 : "a"(num), "r"(__rdi), "r"(__rsi), "r"(__rdx), \
-													 "r"(__r10), "r"(__r8)                         \
-												 : "rcx", "r11", "memory");                      \
-		if (__ret < 0) {                                \
-			errno = -__ret;                               \
-			__ret = -1;                                   \
-		}                                               \
-		__ret;                                                               \
-	})
-#define __syscall6(num, a1, a2, a3, a4, a5, a6)                          \
-	({                                                                     \
-		long long __ret;                                                     \
-		register __typeof__(a1) __rdi __asm__("rdi") = a1;                   \
-		register __typeof__(a2) __rsi __asm__("rsi") = a2;                   \
-		register __typeof__(a3) __rdx __asm__("rdx") = a3;                   \
-		register __typeof__(a4) __r10 __asm__("r10") = a3;                   \
-		register __typeof__(a5) __r8 __asm__("r8") = a5;                     \
-		register __typeof__(a6) __r9 __asm__("r9") = a6;                     \
-		__asm__ __volatile__("syscall"                                       \
-												 : "=a"(__ret)                                   \
-												 : "a"(num), "r"(__rdi), "r"(__rsi), "r"(__rdx), \
-													 "r"(__r10), "r"(__r8), "r"(__r9)              \
-												 : "rcx", "r11", "memory");                      \
-		if (__ret < 0) {                                \
-			errno = -__ret;                               \
-			__ret = -1;                                   \
-		}                                               \
-		__ret;                                                               \
-	})
+static __always_inline long
+__syscall0(long n)
+{
+	unsigned long ret;
+	__asm__ __volatile__("syscall" : "=a"(ret) : "a"(n) : "rcx", "r11", "memory");
+	return ret;
+}
+
+static __always_inline long
+__syscall1(long n, long a1)
+{
+	unsigned long ret;
+	__asm__ __volatile__("syscall"
+											 : "=a"(ret)
+											 : "a"(n), "D"(a1)
+											 : "rcx", "r11", "memory");
+	return ret;
+}
+
+static __always_inline long
+__syscall2(long n, long a1, long a2)
+{
+	unsigned long ret;
+	__asm__ __volatile__("syscall"
+											 : "=a"(ret)
+											 : "a"(n), "D"(a1), "S"(a2)
+											 : "rcx", "r11", "memory");
+	return ret;
+}
+
+static __always_inline long
+__syscall3(long n, long a1, long a2, long a3)
+{
+	unsigned long ret;
+	__asm__ __volatile__("syscall"
+											 : "=a"(ret)
+											 : "a"(n), "D"(a1), "S"(a2), "d"(a3)
+											 : "rcx", "r11", "memory");
+	return ret;
+}
+
+static __always_inline long
+__syscall4(long n, long a1, long a2, long a3, long a4)
+{
+	unsigned long ret;
+	register long r10 __asm__("r10") = a4;
+	__asm__ __volatile__("syscall"
+											 : "=a"(ret)
+											 : "a"(n), "D"(a1), "S"(a2), "d"(a3), "r"(r10)
+											 : "rcx", "r11", "memory");
+	return ret;
+}
+
+static __always_inline long
+__syscall5(long n, long a1, long a2, long a3, long a4, long a5)
+{
+	unsigned long ret;
+	register long r10 __asm__("r10") = a4;
+	register long r8 __asm__("r8") = a5;
+	__asm__ __volatile__("syscall"
+											 : "=a"(ret)
+											 : "a"(n), "D"(a1), "S"(a2), "d"(a3), "r"(r10), "r"(r8)
+											 : "rcx", "r11", "memory");
+	return ret;
+}
+
+static __always_inline long
+__syscall6(long n, long a1, long a2, long a3, long a4, long a5, long a6)
+{
+	unsigned long ret;
+	register long r10 __asm__("r10") = a4;
+	register long r8 __asm__("r8") = a5;
+	register long r9 __asm__("r9") = a6;
+	__asm__ __volatile__("syscall"
+											 : "=a"(ret)
+											 : "a"(n), "D"(a1), "S"(a2), "d"(a3), "r"(r10), "r"(r8),
+												 "r"(r9)
+											 : "rcx", "r11", "memory");
+	return ret;
+}

userspace/signal.c
@@ -6,31 +6,31 @@
 sighandler_t
 signal(int signum, sighandler_t handler)
 {
-	return (sighandler_t)__syscall2(SYS_signal, signum, (long)handler);
+	return (sighandler_t)__syscall_ret(__syscall2(SYS_signal, signum, (long)handler));
 }
 
 int
 kill(pid_t pid, int sig)
 {
-	return __syscall2(SYS_kill, pid, sig);
+	return __syscall_ret(__syscall2(SYS_kill, pid, sig));
 }
 
 int
 sigprocmask(int how, const sigset_t *restrict set, sigset_t *restrict oldset)
 {
-	return __syscall3(SYS_sigprocmask, how, (long)set, (long)oldset);
+	return __syscall_ret(__syscall3(SYS_sigprocmask, how, (long)set, (long)oldset));
 }
 
 int
 sigsuspend(const sigset_t *mask)
 {
-	return __syscall1(SYS_sigsuspend, (long)mask);
+	return __syscall_ret(__syscall1(SYS_sigsuspend, (long)mask));
 }
 
 int
 sigaction(int signum, const struct sigaction *restrict act, struct sigaction *restrict oldact)
 {
-	return __syscall3(SYS_sigaction, signum, (long)act, (long)oldact);
+	return __syscall_ret(__syscall3(SYS_sigaction, signum, (long)act, (long)oldact));
 }
 
 int

userspace/stdio.c
@@ -63,7 +63,7 @@ __fini_stdio(void)
 int
 rename(const char *oldpath, const char *newpath)
 {
-	return __syscall2(SYS_rename, (long)oldpath, (long)newpath);
+	return __syscall_ret(__syscall2(SYS_rename, (long)oldpath, (long)newpath));
 }
 
 static int

userspace/sys_ioctl.c
@@ -39,5 +39,5 @@ ioctl(int fd, unsigned long request, ...)
 		arg = NULL;
 		break;
 	}
-	return __syscall3(SYS_ioctl, fd, request, (long)arg);
+	return __syscall_ret(__syscall3(SYS_ioctl, fd, request, (long)arg));
 }

userspace/sys_mman.c
@@ -5,11 +5,11 @@
 void *
 mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset)
 {
-	return (void *)__syscall6(SYS_mmap, (long)addr, length, prot, flags, fd, offset);
+	return (void *)__syscall_ret(__syscall6(SYS_mmap, (long)addr, length, prot, flags, fd, offset));
 }
 
 int
 munmap(void *addr, size_t length)
 {
-	return __syscall2(SYS_munmap, (long)addr, length);
+	return __syscall_ret(__syscall2(SYS_munmap, (long)addr, length));
 }

userspace/sys_ptrace.c
@@ -5,5 +5,5 @@
 int
 ptrace(char mask[static SYSCALL_AMT])
 {
-	return __syscall1(SYS_ptrace, (long)mask);
+	return __syscall_ret(__syscall1(SYS_ptrace, (long)mask));
 }

userspace/sys_stat.c
@@ -5,25 +5,25 @@
 int
 mknod(const char *pathname, mode_t mode, dev_t device)
 {
-	return __syscall3(SYS_mknod, (long)pathname, mode, device);
+	return __syscall_ret(__syscall3(SYS_mknod, (long)pathname, mode, device));
 }
 
 int
 mkdir(const char *pathname, mode_t mode)
 {
-	return __syscall2(SYS_mkdir, (long)pathname, mode);
+	return __syscall_ret(__syscall2(SYS_mkdir, (long)pathname, mode));
 }
 
 int
 stat(const char *restrict pathname, struct stat *restrict statbuf)
 {
-	return __syscall2(SYS_stat, (long)pathname, (long)statbuf);
+	return __syscall_ret(__syscall2(SYS_stat, (long)pathname, (long)statbuf));
 }
 
 int
 fstat(int fd, struct stat *restrict statbuf)
 {
-	return __syscall2(SYS_fstat, fd, (long)statbuf);
+	return __syscall_ret(__syscall2(SYS_fstat, fd, (long)statbuf));
 }
 
 // This should probably be moved into the kernel, because
@@ -40,19 +40,19 @@ lstat(const char *restrict n, struct stat *restrict st)
 int
 chmod(const char *pathname, mode_t mode)
 {
-	return __syscall2(SYS_chmod, (long)pathname, mode);
+	return __syscall_ret(__syscall2(SYS_chmod, (long)pathname, mode));
 }
 
 int
 fchmod(int fd, mode_t mode)
 {
-	return __syscall2(SYS_fchmod, fd, mode);
+	return __syscall_ret(__syscall2(SYS_fchmod, fd, mode));
 }
 
 mode_t
 umask(mode_t mask)
 {
-	return __syscall1(SYS_umask, mask);
+	return __syscall_ret(__syscall1(SYS_umask, mask));
 }
 
 int

userspace/sys_uio.c
@@ -6,5 +6,5 @@
 ssize_t
 writev(int fd, const struct iovec *iov, int iovcnt)
 {
-	return __syscall3(SYS_writev, fd, (long)iov, iovcnt);
+	return __syscall_ret(__syscall3(SYS_writev, fd, (long)iov, iovcnt));
 }

userspace/sys_wait.c
@@ -5,5 +5,5 @@
 pid_t
 wait3(int *status, int options, struct rusage *rusage)
 {
-	return __syscall3(SYS_wait3, (long)status, options, (long)rusage);
+	return __syscall_ret(__syscall3(SYS_wait3, (long)status, options, (long)rusage));
 }

userspace/ulib.c
@@ -95,8 +95,11 @@ closedir(DIR *dir)
 	while (dir->list != NULL && dir->list->next != NULL) {
 		dir->list = dir->list->next;
 	}
+
 	// Step back one level.
-	dir->list = dir->list->prev;
+	if (dir->list != NULL) {
+		dir->list = dir->list->prev;
+	}
 
 	// Start freeing the list.
 	while (dir->list->prev != NULL) {

userspace/umalloc.c
@@ -1,4 +1,3 @@
-#include "stdint.h"
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,8 +14,8 @@ struct header {
 
 typedef struct header Header;
 
-static Header base;
-static Header *freep;
+static Header base = {NULL, 0};
+static Header *freep = NULL;
 
 void
 free(void *ap)

userspace/unistd.c
@@ -14,156 +14,156 @@
 pid_t
 fork(void)
 {
-	return __syscall0(SYS_fork);
+	return __syscall_ret(__syscall0(SYS_fork));
 }
 
 
 void __noreturn
 _exit(int status)
 {
-	__syscall1(SYS__exit, status);
+	__syscall_ret(__syscall1(SYS__exit, status));
 	unreachable();
 }
 
 pid_t
 wait(int *wstatus)
 {
-	return __syscall1(SYS_wait, (long)wstatus);
+	return __syscall_ret(__syscall1(SYS_wait, (long)wstatus));
 }
 
 int
 pipe(int pipefd[2])
 {
-	return __syscall1(SYS_pipe, (long)pipefd);
+	return __syscall_ret(__syscall1(SYS_pipe, (long)pipefd));
 }
 
 ssize_t
 read(int fd, void *buf, size_t count)
 {
-	return __syscall3(SYS_read, fd, (long)buf, count);
+	return __syscall_ret(__syscall3(SYS_read, fd, (long)buf, count));
 }
 
 ssize_t
 write(int fd, const void *buf, size_t count)
 {
-	return __syscall3(SYS_write, fd, (long)buf, count);
+	return __syscall_ret(__syscall3(SYS_write, fd, (long)buf, count));
 }
 
 int
 close(int fd)
 {
-	return __syscall1(SYS_close, fd);
+	return __syscall_ret(__syscall1(SYS_close, fd));
 }
 
 
 int
 execve(const char *pathname, char *const argv[], char *const envp[])
 {
-	return __syscall3(SYS_execve, (long)pathname, (long)argv, (long)envp);
+	return __syscall_ret(__syscall3(SYS_execve, (long)pathname, (long)argv, (long)envp));
 }
 
 
 int
 unlink(const char *pathname)
 {
-	return __syscall1(SYS_unlink, (long)pathname);
+	return __syscall_ret(__syscall1(SYS_unlink, (long)pathname));
 }
 
 int
 link(const char *oldpath, const char *newpath)
 {
-	return __syscall2(SYS_link, (long)oldpath, (long)newpath);
+	return __syscall_ret(__syscall2(SYS_link, (long)oldpath, (long)newpath));
 }
 
 int
 chdir(const char *path)
 {
-	return __syscall1(SYS_chdir, (long)path);
+	return __syscall_ret(__syscall1(SYS_chdir, (long)path));
 }
 
 int
 dup(int oldfd)
 {
-	return __syscall1(SYS_dup, oldfd);
+	return __syscall_ret(__syscall1(SYS_dup, oldfd));
 }
 
 pid_t
 getpid(void)
 {
-	return __syscall0(SYS_getpid);
+	return __syscall_ret(__syscall0(SYS_getpid));
 }
 
 pid_t
 getppid(void)
 {
-	return __syscall0(SYS_getppid);
+	return __syscall_ret(__syscall0(SYS_getppid));
 }
 
 void *
 sbrk(int increment)
 {
-	return (void *)__syscall1(SYS_sbrk, increment);
+	return (void *)__syscall_ret(__syscall1(SYS_sbrk, increment));
 }
 
 unsigned int
 alarm(unsigned int seconds)
 {
-	return __syscall1(SYS_alarm, seconds);
+	return __syscall_ret(__syscall1(SYS_alarm, seconds));
 }
 
 time_t
 time(time_t *tloc)
 {
-	return __syscall1(SYS_time, (long)tloc);
+	return __syscall_ret(__syscall1(SYS_time, (long)tloc));
 }
 
 int
 setgid(gid_t gid)
 {
-	return __syscall1(SYS_setgid, gid);
+	return __syscall_ret(__syscall1(SYS_setgid, gid));
 }
 
 int
 setuid(uid_t uid)
 {
-	return __syscall1(SYS_setuid, uid);
+	return __syscall_ret(__syscall1(SYS_setuid, uid));
 }
 
 uid_t
 getuid(void)
 {
-	return __syscall0(SYS_getuid);
+	return __syscall_ret(__syscall0(SYS_getuid));
 }
 
 gid_t
 getgid(void)
 {
-	return __syscall0(SYS_getgid);
+	return __syscall_ret(__syscall0(SYS_getgid));
 }
 
 
 int
 symlink(const char *target, const char *linkpath)
 {
-	return __syscall2(SYS_symlink, (long)target, (long)linkpath);
+	return __syscall_ret(__syscall2(SYS_symlink, (long)target, (long)linkpath));
 }
 
 ssize_t
 readlink(const char *restrict pathname, char *restrict linkpath, size_t buf)
 {
-	return __syscall3(SYS_readlink, (long)pathname, (long)linkpath, buf);
+	return __syscall_ret(__syscall3(SYS_readlink, (long)pathname, (long)linkpath, buf));
 }
 
 off_t
 lseek(int fd, off_t offset, int whence)
 {
-	return __syscall3(SYS_lseek, fd, offset, whence);
+	return __syscall_ret(__syscall3(SYS_lseek, fd, offset, whence));
 }
 
 int
 fsync(int fd)
 {
-	return __syscall1(SYS_fsync, fd);
+	return __syscall_ret(__syscall1(SYS_fsync, fd));
 }
 
 char *
@@ -188,7 +188,7 @@ getcwd(char *buf, size_t n)
 		return buf;
 	}
 
-	int ret = __syscall2(SYS_getcwd, (long)buf, n);
+	int ret = __syscall_ret(__syscall2(SYS_getcwd, (long)buf, n));
 	if (ret < 0)
 		return NULL;
 	else
@@ -198,19 +198,19 @@ getcwd(char *buf, size_t n)
 __deprecated("Removed in POSIX.1-2008; use fork()") pid_t
 vfork(void)
 {
-	return __syscall0(SYS_vfork);
+	return __syscall_ret(__syscall0(SYS_vfork));
 }
 
 clock_t
 times(struct tms *buf)
 {
-	return __syscall1(SYS_times, (long)buf);
+	return __syscall_ret(__syscall1(SYS_times, (long)buf));
 }
 
 int
 access(const char *pathname, int mode)
 {
-	return __syscall2(SYS_access, (long)pathname, mode);
+	return __syscall_ret(__syscall2(SYS_access, (long)pathname, mode));
 }
 
 uid_t
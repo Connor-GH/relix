Connor-GH/relix

commit bec35cb5e809794d57b7e706dd29b7093ccb429d

Replace int $x40 syscalls with the x86 syscall/sysret pair
preliminary benchmarks show a performance improvement of 30%. Right now, release builds are broken due to a page fault in init, but it will be fixed next commit.

kernel/include/proc.h
@@ -20,9 +20,12 @@ struct cred {
 };
 
 struct cpu {
+	struct cpu *self;
+	uint64_t kernel_stack;
+	uint64_t user_stack;
 	uint8_t apicid; // Local APIC ID
 	struct context *scheduler; // swtch() here to enter scheduler
-	struct taskstate64 *tss; // Used by x86 to find stack for interrupt
+	struct taskstate64 *tss __attribute__((aligned(16))); // Used by x86 to find stack for interrupt
 	union {
 		struct segdesc gdt[NSEGS]; // x86 global descriptor table
 #if X86_64

kernel/include/syscall.h
@@ -127,6 +127,8 @@ argunsigned_long(int, unsigned long *);
 int
 arguintptr_t(int n, uintptr_t *ip);
 int
+argintptr_t(int n, intptr_t *ip);
+int
 argssize_t(int n, ssize_t *sp);
 int
 argsize_t(int n, size_t *sp);
@@ -152,6 +154,10 @@ ssize_t
 fetchstr(uintptr_t, char **);
 void
 syscall(void);
+
+// Init function for syscall/sysret.
+void
+syscall_init(void);
 
 /*
  * Designed for functions that return int, where

kernel/proc.c
@@ -7,6 +7,7 @@
 #include <stddef.h>
 #include <string.h>
 #include "drivers/mmu.h"
+#include "kernel_assert.h"
 #include "drivers/lapic.h"
 #include "defs.h"
 #include "param.h"
@@ -22,7 +23,7 @@
 #include "file.h"
 #include "fs.h"
 #include "trap.h"
-#include "compiler_attributes.h"
+#include "lib/compiler_attributes.h"
 #include "types.h"
 #include "kernel_signal.h"
 
@@ -55,6 +56,12 @@ int
 my_cpu_id(void)
 {
 	pushcli();
+	// This is a silly trick:
+	// mycpu() outputs a "struct cpu *"
+	// cpus is of type "struct cpu *"
+	// mycpu() returns an offset of cpus,
+	// i.e. (cpus + id)
+	// (cpus + id) - cpus == id.
 	int ret = mycpu() - cpus;
 	popcli();
 	return ret;
@@ -69,14 +76,16 @@ mycpu(void)
 	int apicid, i;
 
 	if (readrflags() & FL_IF)
-		panic("mycpu called with interrupts enabled\n");
+		panic("mycpu called with interrupts enabled");
 
 	apicid = lapicid();
 	// APIC IDs are not guaranteed to be contiguous. Maybe we should have
 	// a reverse map, or reserve a register to store &cpus[i].
 	for (i = 0; i < ncpu; ++i) {
-		if (cpus[i].apicid == apicid)
+		if (cpus[i].apicid == apicid) {
+			kernel_assert(cpus[i].apicid == i);
 			return &cpus[i];
+		}
 	}
 	// apicid is not familiar; we are probably in the early
 	// stages of booting. just use the first CPU for now.
@@ -85,7 +94,7 @@ mycpu(void)
 	pass++;
 	if (pass < 200000)
 		return &cpus[0];
-	panic("unknown apicid\n");
+	panic("unknown apicid");
 }
 
 // Disable interrupts so that we are not rescheduled
@@ -673,8 +682,9 @@ kernel_attach_signal(int signum, sighandler_t handler)
 	if (signum >= __SIG_last || signum < 0) {
 		return SIG_ERR;
 	} else {
+		sighandler_t old = myproc()->sig_handlers[signum];
 		myproc()->sig_handlers[signum] = handler;
-		return NULL;
+		return old;
 	}
 }
 

kernel/syscall.c
@@ -1,15 +1,20 @@
+#include "mmu.h"
 #include "param.h"
 #include "spinlock.h"
 #include "memlayout.h"
 #include "trap.h"
 #include <errno.h>
+#include "kernel_assert.h"
 #include <fcntl.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <defs.h>
 #include "proc.h"
 #include "x86.h"
 #include "syscall.h"
 #include "console.h"
+#include "msr.h"
+#include <stdnoreturn.h>
 
 #define SYSCALL_ARG_FETCH(T)                                   \
 	int fetch##T(uintptr_t addr, T *ip)                          \
@@ -67,7 +72,7 @@ fetcharg(int n)
 	case 2:
 		return proc->tf->rdx;
 	case 3:
-		return proc->tf->rcx;
+		return proc->tf->r10;
 	case 4:
 		return proc->tf->r8;
 	case 5:
@@ -87,6 +92,7 @@ SYSCALL_ARG_N(int);
 SYSCALL_ARG_N(unsigned_int);
 SYSCALL_ARG_N(unsigned_long);
 SYSCALL_ARG_N(uintptr_t);
+SYSCALL_ARG_N(intptr_t);
 SYSCALL_ARG_N(ssize_t);
 SYSCALL_ARG_N(size_t);
 SYSCALL_ARG_N(off_t);
@@ -284,6 +290,135 @@ static size_t (*syscalls[])(void) = {
 	[SYS_access] = sys_access,
 	[SYS_fcntl] = sys_fcntl,
 };
+
+noreturn static void
+syscall_do(void);
+
+__attribute__((noinline)) void
+syscall_init(void)
+{
+	// Tell the EFER to enable syscall/sysret.
+	wrmsr(MSR_EFER, rdmsr(MSR_EFER) | EFER_SCE);
+	// Show LSTAR where the syscall function is.
+	wrmsr(MSR_LSTAR, (uintptr_t)&syscall_do);
+	uint64_t star = 0;
+	// For the "target sysret" mode (user mode), %ss is 63..48 + 16.
+	// This is because they expect your GDT to look like the following:
+	// KCODE
+	// KDATA
+	// UCODE32
+	// UDATA (previously: UDATA32 before IA-32e, which is amd64)
+	// [something]
+	// UCODE64
+	//
+	// UCODE32 + 16 == UCODE64
+	// UCODE + 8 == UDATA
+	//
+	// Our GDT looks like the following:
+	// KCODE
+	// KDATA
+	// UDATA
+	// UCODE
+	// and so we do "-16" to set the ss at UCODE,
+	// and this results in a "-8" for UDATA.
+	//
+	// And the compatability mode uses UCODE32 + 0 == UCODE32.
+	star |= ((((unsigned long)SEG_UCODE << 3UL) - 16) | DPL_USER) << 48UL;
+	star |= ((((unsigned long)SEG_KCODE << 3UL)) | DPL_KERNEL) << 32UL;
+
+	wrmsr(MSR_STAR, star);
+	wrmsr(MSR_FMASK, FL_DIRECTION | FL_IF | FL_TRAP);
+	wrmsr(MSR_KERNEL_GS_BASE,(uint64_t)mycpu());
+}
+
+void
+syswrap(struct trapframe *tf);
+
+noreturn __attribute__((naked))
+static void
+syscall_do(void)
+{
+	__asm__ __volatile__(
+		"swapgs\n" // user %gs -> kernel %gs
+
+		"mov %%rsp, %%gs:%c[user_stack]\n"
+		"mov %%gs:%c[kernel_stack], %%rsp\n"
+
+		"pushq $0\n" // ss
+
+		"pushq %%gs:%c[user_stack]\n" // user rsp.
+		// We used %gs for all we need it for (CPU-local data).
+		// Swap it back.
+		"swapgs\n"
+		"sti\n"
+
+		"pushq %%r11\n" // rflags
+		"movq %%cs, %%r11\n"
+		"pushq %%r11\n" // cs
+		"pushq %%rcx\n" // user rip is preserved in rcx.
+		"pushq $0\n" // err
+		"pushq $0\n" // trapno
+		"pushq %%r15\n"
+		"pushq %%r14\n"
+		"pushq %%r13\n"
+		"pushq %%r12\n"
+		"pushq $0\n" // r11 was trashed (rflags)
+		"pushq %%r10\n" // r10 is caller-save
+		"pushq %%r9\n" // r9 is caller-save
+		"pushq %%r8\n" // r8 is caller-save and scratch
+		"pushq %%rdi\n" // rdi is caller-save
+		"pushq %%rsi\n" // rsi is caller-save
+		"pushq %%rbp\n"
+		"pushq %%rdx\n" // rdx is caller-save
+		"pushq $0\n" // rcx was trashed (rip)
+		"pushq %%rbx\n"
+		"pushq %%rax\n"
+
+		"xor %%rbp, %%rbp\n"
+		"movq %%rsp, %%rdi\n"
+		"callq syswrap\n"
+
+		"popq %%rax\n"
+		"popq %%rbx\n"
+		"addq $8, %%rsp\n"
+		"popq %%rdx\n"
+		"popq %%rbp\n"
+		"popq %%rsi\n"
+		"popq %%rdi\n"
+		"popq %%r9\n"
+		"popq %%r8\n"
+		"popq %%r10\n"
+		"popq %%r11\n"
+		"popq %%r12\n"
+		"popq %%r13\n"
+		"popq %%r14\n"
+		"popq %%r15\n"
+
+		"addq $(8 * 2), %%rsp\n" // skip trapno and err
+		"popq %%rcx\n" // rip
+		"addq $8, %%rsp\n" // skip cs
+		"popq %%r11\n" // rflags
+
+		"cli\n"
+		"popq %%rsp\n" // rsp
+		// TODO stack leak of %ss?
+
+		"sysretq\n"
+		:
+		: [user_stack] "i" (offsetof(struct cpu, user_stack)),
+			[kernel_stack] "i" (offsetof(struct cpu, kernel_stack))
+	);
+}
+_Static_assert(offsetof(struct cpu, user_stack) == 16, "");
+_Static_assert(offsetof(struct cpu, kernel_stack) == 8, "");
+
+void
+syswrap(struct trapframe *tf)
+{
+	kernel_assert(myproc() != NULL);
+	myproc()->tf = tf;
+	syscall();
+}
 
 void
 syscall(void)

kernel/sysproc.c
@@ -182,9 +182,7 @@ sys_setgid(void)
 	uid_t gid;
 	if (argint(0, &gid) < 0)
 		return -EINVAL;
-	struct cred cred;
-	cred.gid = gid;
-	myproc()->cred = cred;
+	myproc()->cred.gid = gid;
 	return 0;
 }
 
@@ -197,9 +195,7 @@ sys_setuid(void)
 	uid_t uid;
 	if (argint(0, &uid) < 0)
 		return -EINVAL;
-	struct cred cred;
-	cred.uid = uid;
-	myproc()->cred = cred;
+	myproc()->cred.uid = uid;
 	return 0;
 }
 
@@ -231,10 +227,8 @@ sys_signal(void)
 {
 	int signum;
 	sighandler_t handler;
-	if (argint(0, &signum) < 0)
-		return -EINVAL;
-	if (argptr(1, (char **)&handler, sizeof(*handler)) < 0)
-		return -EINVAL;
+	PROPOGATE_ERR(argint(0, &signum));
+	PROPOGATE_ERR(argptr(1, (char **)&handler, sizeof(*handler)));
 	return (size_t)kernel_attach_signal(signum, handler);
 }
 
@@ -275,5 +269,5 @@ sys_times(void)
 	if (argptr(0, (char **)&tms, sizeof(struct tms *)) < 0)
 		return -EFAULT;
 
-	return 1;
+	return -ENOSYS;
 }

userspace/unistd.c
@@ -1,7 +1,229 @@
+#include "limits.h"
+#include <sys/types.h>
+#include <sys/times.h>
+#include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <dirent.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <sys/syscall.h>
+#include "libc_syscalls.h"
+
+pid_t
+fork(void)
+{
+	return __syscall0(SYS_fork);
+}
+
+
+void __noreturn
+_exit(int status)
+{
+	__syscall1(SYS__exit, status);
+	unreachable();
+}
+
+pid_t
+wait(int *wstatus)
+{
+	return __syscall1(SYS_wait, (long)wstatus);
+}
+
+int
+pipe(int pipefd[2])
+{
+	return __syscall1(SYS_pipe, (long)pipefd);
+}
+
+ssize_t
+read(int fd, void *buf, size_t count)
+{
+	return __syscall3(SYS_read, fd, (long)buf, count);
+}
+
+ssize_t
+write(int fd, const void *buf, size_t count)
+{
+	return __syscall3(SYS_write, fd, (long)buf, count);
+}
+
+int
+close(int fd)
+{
+	return __syscall1(SYS_close, fd);
+}
+
+
+int
+execve(const char *pathname, char *const argv[], char *const envp[])
+{
+	return __syscall3(SYS_execve, (long)pathname, (long)argv, (long)envp);
+}
+
+
+int
+unlink(const char *pathname)
+{
+	return __syscall1(SYS_unlink, (long)pathname);
+}
+
+int
+link(const char *oldpath, const char *newpath)
+{
+	return __syscall2(SYS_link, (long)oldpath, (long)newpath);
+}
+
+int
+chdir(const char *path)
+{
+	return __syscall1(SYS_chdir, (long)path);
+}
+
+int
+dup(int oldfd)
+{
+	return __syscall1(SYS_dup, oldfd);
+}
+
+pid_t
+getpid(void)
+{
+	return __syscall0(SYS_getpid);
+}
+
+pid_t
+getppid(void)
+{
+	return __syscall0(SYS_getppid);
+}
+
+void *
+sbrk(int increment)
+{
+	return (void *)__syscall1(SYS_sbrk, increment);
+}
+
+unsigned int
+alarm(unsigned int seconds)
+{
+	return __syscall1(SYS_alarm, seconds);
+}
+
+time_t
+time(time_t *tloc)
+{
+	return __syscall1(SYS_time, (long)tloc);
+}
+
+int
+setgid(gid_t gid)
+{
+	return __syscall1(SYS_setgid, gid);
+}
+
+int
+setuid(uid_t uid)
+{
+	return __syscall1(SYS_setuid, uid);
+}
+
+uid_t
+getuid(void)
+{
+	return __syscall0(SYS_getuid);
+}
+
+gid_t
+getgid(void)
+{
+	return __syscall0(SYS_getgid);
+}
+
+
+int
+symlink(const char *target, const char *linkpath)
+{
+	return __syscall2(SYS_symlink, (long)target, (long)linkpath);
+}
+
+ssize_t
+readlink(const char *restrict pathname, char *restrict linkpath, size_t buf)
+{
+	return __syscall3(SYS_readlink, (long)pathname, (long)linkpath, buf);
+}
+
+off_t
+lseek(int fd, off_t offset, int whence)
+{
+	return __syscall3(SYS_lseek, fd, offset, whence);
+}
+
+int
+fsync(int fd)
+{
+	return __syscall1(SYS_fsync, fd);
+}
+
+char *
+getcwd(char *buf, size_t n)
+{
+	// POSIX leaves this the behavior on NULL buf
+	// unspecified, but bash needs this in order to
+	// not deref a NULL pointer. Objectively, this
+	// is bash's fault for not following the standard,
+	// but I will make an exception this *once*.
+	if (buf == NULL) {
+		size_t alloc_size;
+		if (n == 0)
+			alloc_size = PATH_MAX;
+		else
+			alloc_size = n;
+
+		buf = malloc(alloc_size);
+		if (buf == NULL)
+			return NULL;
+
+		return buf;
+	}
+
+	int ret = __syscall2(SYS_getcwd, (long)buf, n);
+	if (ret < 0)
+		return NULL;
+	else
+		return buf;
+}
+
+__deprecated("Removed in POSIX.1-2008; use fork()") pid_t
+vfork(void)
+{
+	return __syscall0(SYS_vfork);
+}
+
+clock_t
+times(struct tms *buf)
+{
+	return __syscall1(SYS_times, (long)buf);
+}
+
+int
+access(const char *pathname, int mode)
+{
+	return __syscall2(SYS_access, (long)pathname, mode);
+}
+
+uid_t
+geteuid(void)
+{
+	return getuid();
+}
+
+uid_t
+getegid(void)
+{
+	return getgid();
+}
 
 static char ttyname_buf[FILENAME_MAX];
 

include/sys/syscall.h
@@ -1,10 +1,6 @@
 #pragma once
 #include "kernel/include/syscall.h"
 #undef __KERNEL__
-#include "kernel/include/traps.h"
-#define STRINGIFY(s) #s
-#define BOILERPLATE(s) STRINGIFY(s)
-#define SC_STRING BOILERPLATE(T_SYSCALL)
 
 #define __GET_MACRO(_0, _1, _2, _3, _4, _5, _6, x, ...) x
 #define syscall(...)                                                       \

commit 6aeeceb0f26f668402ac1385710ad8a96a10204d

Fix regression caused in bec35cb5e809794d57b7e706dd29b7093ccb429d and remove faulty vfork()
... vfork() is deprecated anyways. Also, the regression caused by bec35cb5e809794d57b7e706dd29b7093ccb429d was arbitrary read of kernel memory because the CS and SS registers were not set correctly :(

kernel/include/proc.h
@@ -107,7 +107,7 @@ struct proc {
 
 int my_cpu_id(void);
 void exit(int) __attribute__((noreturn));
-pid_t fork(bool virtual);
+pid_t fork(void);
 int growproc(intptr_t);
 int kill(pid_t, int);
 struct cpu *mycpu(void);

kernel/include/syscall.h
@@ -39,7 +39,7 @@
 #define SYS_signal 36
 #define SYS_getcwd 37
 #define SYS_sigprocmask 38
-#define SYS_vfork 39
+#define SYS_UNMAPPED1 39
 #define SYS_wait3 40
 #define SYS_sigsuspend 41
 #define SYS_umask 42
@@ -98,7 +98,7 @@ __attribute__((unused)) static const char *syscall_names[SYSCALL_AMT + 1] = {
 	[SYS_signal] = "signal",
 	[SYS_getcwd] = "getcwd",
 	[SYS_sigprocmask] = "sigprocmask",
-	[SYS_vfork] = "vfork",
+	[SYS_UNMAPPED1] = "unmapped1",
 	[SYS_wait3] = "wait3",
 	[SYS_sigsuspend] = "sigsuspend",
 	[SYS_umask] = "umask",

kernel/proc.c
@@ -248,7 +248,7 @@ growproc(intptr_t n)
 // Caller must set state of returned proc to RUNNABLE.
 // POSIX.1-2008: fork returns a "signed integer type".
 pid_t
-fork(bool virtual)
+fork(void)
 {
 	pid_t pid;
 	struct proc *np;
@@ -267,20 +267,18 @@ fork(bool virtual)
 		return -EIO;
 	}
 	np->sz = curproc->sz;
-	// Only do this for regular fork().
-	if (!virtual) {
-		np->heap = curproc->heap;
-		np->heapsz = curproc->heapsz;
 
-		np->mmap_count = curproc->mmap_count;
-		memcpy(np->mmap_info, curproc->mmap_info, sizeof(np->mmap_info));
-		// Copying the info isn't enough. We also need to map it.
-		for (int j = 0; np->mmap_info[j].length != 0; j++) {
-			struct mmap_info info = np->mmap_info[j];
-			if (mappages(np->pgdir, (void *)info.virt_addr, info.length, info.addr,
-			             info.perm) < 0) {
-				panic("Could not map page");
-			}
+	np->heap = curproc->heap;
+	np->heapsz = curproc->heapsz;
+
+	np->mmap_count = curproc->mmap_count;
+	memcpy(np->mmap_info, curproc->mmap_info, sizeof(np->mmap_info));
+	// Copying the info isn't enough. We also need to map it.
+	for (int j = 0; np->mmap_info[j].length != 0; j++) {
+		struct mmap_info info = np->mmap_info[j];
+		if (mappages(np->pgdir, (void *)info.virt_addr, info.length, info.addr,
+		             info.perm) < 0) {
+			panic("Could not map page");
 		}
 	}
 	np->parent = curproc;

kernel/syscall.c
@@ -170,7 +170,6 @@ extern size_t sys_munmap(void);
 extern size_t sys_signal(void);
 extern size_t sys_getcwd(void);
 extern size_t sys_sigprocmask(void);
-extern size_t sys_vfork(void);
 extern size_t sys_wait3(void);
 extern size_t sys_sigsuspend(void);
 extern size_t sys_umask(void);
@@ -185,6 +184,17 @@ extern size_t sys_fchmod(void);
 extern size_t sys_faccessat(void);
 extern size_t sys_fcntl(void);
 extern size_t sys_uname(void);
+
+static size_t
+unknown_syscall(void)
+{
+	struct proc *curproc = myproc();
+	uint64_t num = curproc->tf->rax;
+
+	uart_printf("%d %s: unknown sys call %lu\n", curproc->pid, curproc->name,
+	            num);
+	return -1;
+}
 
 static size_t (*syscalls[])(void) = {
 	[SYS_fork] = sys_fork,
@@ -225,7 +235,7 @@ static size_t (*syscalls[])(void) = {
 	[SYS_signal] = sys_signal,
 	[SYS_getcwd] = sys_getcwd,
 	[SYS_sigprocmask] = sys_sigprocmask,
-	[SYS_vfork] = sys_vfork,
+	[SYS_UNMAPPED1] = unknown_syscall,
 	[SYS_wait3] = sys_wait3,
 	[SYS_sigsuspend] = sys_sigsuspend,
 	[SYS_umask] = sys_umask,
@@ -292,7 +302,7 @@ syscall_do(void)
 		"mov %%rsp, %%gs:%c[user_stack]\n"
 		"mov %%gs:%c[kernel_stack], %%rsp\n"
 
-		"pushq $0\n" // ss
+		"pushq $0x1b\n" // ss
 
 		"pushq %%gs:%c[user_stack]\n" // user rsp.
 		// We used %gs for all we need it for (CPU-local data).
@@ -301,8 +311,7 @@ syscall_do(void)
 		"sti\n"
 
 		"pushq %%r11\n" // rflags
-		"movq %%cs, %%r11\n"
-		"pushq %%r11\n" // cs
+		"pushq $0x23\n" // cs
 		"pushq %%rcx\n" // user rip is preserved in rcx.
 		"pushq $0\n" // err
 		"pushq $0\n" // trapno
@@ -394,7 +403,6 @@ syscall(void)
 			curproc->tf->rax = syscalls[num]();
 		}
 	} else {
-		cprintf("%d %s: unknown sys call %lu\n", curproc->pid, curproc->name, num);
-		curproc->tf->rax = -1;
+		curproc->tf->rax = unknown_syscall();
 	}
 }

kernel/sysproc.c
@@ -19,7 +19,7 @@
 size_t
 sys_fork(void)
 {
-	return fork(false);
+	return fork();
 }
 
 size_t
@@ -234,13 +234,6 @@ size_t
 sys_sigprocmask(void)
 {
 	return -ENOSYS;
-}
-
-// TODO implement properly
-size_t
-sys_vfork(void)
-{
-	return fork(true);
 }
 
 size_t

userspace/unistd.c
@@ -312,7 +312,8 @@ getcwd(char *buf, size_t n)
 
 __deprecated("Removed in POSIX.1-2008; use fork()") pid_t vfork(void)
 {
-	return __syscall_ret(__syscall0(SYS_vfork));
+	errno = ENOSYS;
+	return -1;
 }
 
 clock_t
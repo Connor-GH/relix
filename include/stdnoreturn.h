4244d5f6797efbbd62ca01dc309e92b7353e5c13
Connor-GH <connor-gh@outlook.com>
2025-09-26 12:13:27 -0500

    Update Relix's C standard to C23 (gnu23)
    ... over time, the code will adopt C23-isms. We were already using some like binary literals, but this expands the scope of what we can do.

--- include/stdnoreturn.h
+++ include/stdnoreturn.h
@@ -1,4 +1,6 @@
 #pragma once
 #if __STDC_VERSION__ < 202311L
 #define noreturn _Noreturn
+#else
+#define noreturn [[noreturn]]
 #endif
---
+++ userspace/libc/__libc_start_main.c
@@ -0,0 +1,48 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2025 Connor-GH. All Rights Reserved.
+ */
+#include <stdlib.h>
+#include <unistd.h>
+
+// TODO: we should call _init() in __libc_start_main().
+
+void __fini_stdio(void);
+static void
+cleanup(void)
+{
+	__fini_stdio();
+}
+
+void __init_stdio(void);
+
+static void
+startup(void)
+{
+	__init_stdio();
+}
+
+char **environ;
+
+// Standard says that main can't have a prototype (N3220 5.1.2.3.2),
+// so we funnel main's pointer through this parameter in __libc_start_main.
+// Of course, if an application specifies main(void), the arguments get
+// added to the registers but never used, which isn't a huge problem.
+// I don't see a way of doing this purely in C.
+void
+__libc_start_main(int argc, char **argv, char **envp,
+                  int (*mainfunc)(int argc, char **argv, char **envp))
+{
+	environ = envp;
+	optind = 1;
+	opterr = 1;
+
+	startup();
+	atexit(cleanup);
+	/* Possible parameters:
+	 * - int argc, char **argv, char **envp
+	 * - int argc, char **argv
+	 * - void
+	 */
+	exit(mainfunc(argc, argv, envp));
+}

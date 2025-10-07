d23c4a54fc4af9f709bec638df6b642c719dde9f
Connor-GH <connor-gh@outlook.com>
2025-10-07 12:03:11 -0500

    wchar_t should be in stddef.h

--- include/stddef.h
+++ include/stddef.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <bits/__NULL.h>
 #include <bits/size_t.h>
+#include <bits/wchar_t.h>
 #define offsetof(x, y) __builtin_offsetof(x, y)
 #define NULL __NULL
 typedef __size_t size_t;
 typedef __PTRDIFF_TYPE__ ptrdiff_t;
+typedef __wchar_t wchar_t;
 #define unreachable() (__builtin_unreachable())
